bf6f9d82e0703397fbdd9fe428c677045fbe8ac9
Brendan Koral <brendanhkoral@gmail.com>
2017-03-12 22:57:33 -0500

    Some User class process using a loop to auto fill data into an array

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -8,17 +8,29 @@ class User {
     public $first_name;
     public $last_name;
 
-    public static function instantiation($found_user) {
+    public static function instantiation($the_record) {
 
         $the_object = new self;
 
-        $the_object->id = $found_user['id'];
-        $the_object->first_name = $found_user['first_name'];
-        $the_object->last_name = $found_user['last_name'];
-        $the_object->password = $found_user['password'];
-        $the_object->username = $found_user['username'];
+        foreach ($the_record as $the_attribute => $value) {
+
+            if($the_object->has_the_attribute($the_attribute) ) {
+
+                $the_object->the_attribute = $value;
+
+            }
+
+        }
 
         return $the_object;
+
+    }
+
+    private function has_the_attribute($the_attribute) {
+
+        $object_properties = get_object_vars($this);
+
+        return array_key_exists($the_attribute, $object_properties);
 
     }
 
@@ -46,7 +58,15 @@ class User {
 
         $result_set = $database->query($query);
 
-        return $result_set;
+        $the_object_array = array();
+
+        while( $row = mysqli_fetch_array($result_set) ) {
+
+            $the_object_array[] = self::instantiation($row);
+
+        }
+
+        return $the_object_array;
 
     }
 
